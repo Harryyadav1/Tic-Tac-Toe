03a292c2787e78520fc4c1d9c12f0410bbb7f90b
Pawan Yadav <153371267+Harryyadav1@users.noreply.github.com>
2023-12-09 19:48:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board):
+    # Check rows
+    for row in board:
+        if row.count(row[0]) == 3 and row[0] != ' ':
+            return True
+
+    # Check columns
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != ' ':
+            return True
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != ' ':
+        return True
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != ' ':
+        return True
+
+    return False
+
+def is_board_full(board):
+    for row in board:
+        if ' ' in row:
+            return False
+    return True
+
+def tic_tac_toe():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    current_player = 'X'
+
+    while True:
+        print_board(board)
+
+        row = int(input(f"Player {current_player}, enter row (0, 1, or 2): "))
+        col = int(input(f"Player {current_player}, enter column (0, 1, or 2): "))
+
+        if board[row][col] == ' ':
+            board[row][col] = current_player
+        else:
+            print("Cell already occupied. Try again.")
+            continue
+
+        if check_winner(board):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            break
+
+        if is_board_full(board):
+            print_board(board)
+            print("It's a draw!")
+            break
+
+        # Switch player
+        current_player = 'O' if current_player == 'X' else 'X'
+
+if __name__ == "__main__":
+    tic_tac_toe()
+
